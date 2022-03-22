85768f81c14cb2daa0ae9d67cfdeaaf697f817ad
Vidheyakumar13 <95712544+Vidheyakumar13@users.noreply.github.com>
2022-03-22 19:11:26 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+var app = express();
+
+app.set('view-engine', 'ejs')
+
+app.use(express.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({extended : true}));
+
+var task = ["car", "bike", "home"];
+var complete = ["snacks"]; 
+
+app.get('/', (req, res)=>{
+    res.render('/media/ubuntu/6AF240D1F240A2E5/Learning/todo/views/index.ejs')
+})
+app.post('/addtask' , (req, res) => {
+    // var newTask = req.body.newTask
+    // task.push(newTask);
+    // res.redirect("/");
+    console.log(req.body);
+})
+
+
+
+
+app.listen(3000)
