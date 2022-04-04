97baff1fcc684e8a21b55cf105b01aff8ba59699
Vidheyakumar13 <95712544+Vidheyakumar13@users.noreply.github.com>
2022-04-04 13:17:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+let express = require("express")
+let bodyParser = require("body-parser")
+let Port = 9000;
+
+let app = express();
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+let todo = [{"id" : 1 , "task": "buy milk"}, {"id" :  2, "task": "buy water"}, {"id" : 3 , "task": "buy juice"}];
+app.get('/', (req, res) =>{
+    res.send(todo);
+})
+app.get('/:id', (req,res)=>{
+  let task = req.params.id;
+  res.send(task.todo)
+})
+app.post('/', (req, res) => {
+    let newtodo = req.body;
+    newtodo.id = todo.length +1; 
+    todo.push(newtodo);
+    res.send(todo)
+})
+
+app.put('/:id', (req, res) => {
+  const index = todo.findIndex(item => item.id === Number (req.params.id));// retrieve the id parameter value
+  console.log(index, req.params.id, typeof req.params.id);
+  if (todo[index]){
+    const newTodo=req.body;
+    newTodo.id=Number(req.params.id)
+    todo[index]=newTodo
+    // res.status(204).send("Task Updated");
+    console.log(todo[index]);
+    res.json(todo[index])
+  }else{
+    res.status(404).send('The task is not found');
+  }
+});
+  app.delete('/:id', (req, res) => {
+    let index = todo.findIndex(item => item.id === Number(req.params.id));
+    console.log(index, req.params.id, typeof req.params.id)
+    todo.splice(index, 1);
+    res.status(200).send(todo);
+   });
+app.listen(Port);
